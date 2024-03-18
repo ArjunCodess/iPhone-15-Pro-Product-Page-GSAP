6b63b6ad5c5cdd5e6ebf46df9077d3b2f78edce0
Arjun Vijay Prakash <137415649+ArjunCodess@users.noreply.github.com>
2024-03-18 09:19:15 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Highlights from './components/Highlights';
 import Model from './components/Model';
 import Features from './components/Features';
 import HowItWorks from './components/HowItWorks';
+import Footer from './components/Footer';
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
         <Model />
         <Features />
         <HowItWorks />
+        <Footer />
       </main>
     </>
   )
